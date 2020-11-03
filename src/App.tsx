ab808c17f90f541fa15e88086dd72bd51c7de180
adekunle <adekunle@deimos.co.za>
2020-11-03 20:43:17 +0100

    setup project base structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.scss'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useRouteMatch,
+} from 'react-router-dom'
+import Index from 'routes/Index'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Index />
+          </Route>
+        </Switch>
+      </Router>
+      <button className='button is-primary'>HELLO</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/routes/Index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Index = () => {
+  return <div>Index page</div>
+}
+
+export default Index
